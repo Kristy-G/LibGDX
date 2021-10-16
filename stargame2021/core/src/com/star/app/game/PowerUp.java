f45f7187f7f14b0fee9cd5c23b8a63154531de5f
Kristy-G <69619683+Kristy-G@users.noreply.github.com>
2021-10-16 20:49:45 +0700

    L_7

--- stargame2021/core/src/com/star/app/game/PowerUp.java
+++ stargame2021/core/src/com/star/app/game/PowerUp.java
@@ -23,10 +23,6 @@ public class PowerUp implements Poolable {
     private Type type;
     private int power;
 
-    public void setVelocity(Vector2 velocity) {
-        this.velocity = velocity;
-    }
-
     public Type getType() {
         return type;
     }
